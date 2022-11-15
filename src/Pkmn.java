ad17e315fc42c7cdd17fcd7747b566f45c8ad6d8
ClayClimber <leonardo.zattara@libero.it>
2022-11-15 23:42:38 +0100

    feat: inseriti metodi di combattimento

--- src/Pkmn.java
+++ src/Pkmn.java
@@ -1,9 +1,33 @@
 public class Pkmn {
+    private int puntiSalute;
     private int attack;
     private int defense;
 
-    public Pkmn(int attack, int defense){
+    public Pkmn(int puntiSalute, int attack, int defense){
+        this.puntiSalute = puntiSalute;
         this.attack = attack;
         this.defense = defense;
+    }
+
+    public void danni(int attackPoints){
+        int healthPoints;
+        healthPoints = (attackPoints/defense)/50+2;
+        puntiSalute = puntiSalute-healthPoints;
+    }
+
+    public void setPuntiSalute(int puntiSalute) {
+        this.puntiSalute = puntiSalute;
+    }
+
+    public int getPuntiSalute() {
+        return puntiSalute;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
     }
 }
