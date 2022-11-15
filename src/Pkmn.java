44a37d7004a5b7ec0d5e14dabfe9d07ca030b14e
ClayClimber <leonardo.zattara@libero.it>
2022-11-15 19:56:42 +0100

    Initial Commit

---
+++ src/Pkmn.java
@@ -0,0 +1,9 @@
+public class Pkmn {
+    private int attack;
+    private int defense;
+
+    public Pkmn(int attack, int defense){
+        this.attack = attack;
+        this.defense = defense;
+    }
+}
