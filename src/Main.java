e45d3830c52866cb70d2a4887769a3b40cf86362
LEONARDO.ZATTARA <LEONARDO.ZATTARA@buonarroti.tn.it>
2022-11-16 16:14:38 +0100

    test: effettuato con successo il combattimento con arma

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Pkmn trevenant = new Pkmn(250, 85, 50);
-        Pkmn palossand = new Pkmn(300, 40, 120);
-        trevenant.danni(palossand.getAttack());
+        Weapon a1 = new Weapon(100);
+        Pkmn trevenant = new Pkmn(250, 85, 50, a1);
+        Pkmn palossand = new Pkmn(300, 40, 120, a1);
+        trevenant.danni(palossand.getAttack()+palossand.getArma().getAtkStat());
         System.out.println("PS: "+trevenant.getPuntiSalute());
     }
 }
