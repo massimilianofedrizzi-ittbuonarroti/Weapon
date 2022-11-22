753edc532d9171c2ad6e46df4476bfe684688703
max.fedrizzi <max.fedrizzi@buonarroti.tn.it>
2022-11-22 21:28:51 +0100

    feat: creati metodi subisciDanno e arrecaDanno

--- src/Pkmn.java
+++ src/Pkmn.java
@@ -2,22 +2,32 @@ public class Pkmn {
     private int puntiSalute;
     private int attack;
     private int defense;
-
     private Weapon arma;
+    private boolean armed;
 
     public Pkmn(int puntiSalute, int attack, int defense, Weapon arma){
         this.puntiSalute = puntiSalute;
         this.attack = attack;
         this.defense = defense;
         this.arma = arma;
+        this.armed = true;
     }
 
-    public void danni(int attackPoints){
+    public void subisciDanni(int attackPoints){
         int healthPoints;
         healthPoints = (attackPoints*100/defense)/5+2;
         puntiSalute = puntiSalute-healthPoints;
     }
-
+    public int arrecaDanni(){
+        int danno;
+        if(armed){
+            danno=attack+arma.getAtkStat();
+        }
+        else{
+            danno=attack;
+        }
+        return danno;
+    }
     public void setPuntiSalute(int puntiSalute) {
         this.puntiSalute = puntiSalute;
     }
